057f5534ba6d10680786218303066ce3936acec3
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2020-05-25 13:18:58 -0700

    Add mypy type-checking
    Fix type-checking error and add type checking as part of CI.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-.PHONY: black-check, install, isort-check, lint-check, pytest
+.PHONY: black-check, install, isort-check, lint-check, pytest, type-check
 
 PY_TEST_FILES = $(shell find tests -name '*.py')
 PY_FILES = $(shell find gemini -name '*.py')
 
-check: isort-check black-check lint-check pytest
+check: isort-check black-check lint-check type-check pytest
 
 lint-check:
 	poetry run pylint --rcfile=.pylintrc $(PY_FILES)
@@ -17,6 +17,9 @@ pytest:
 
 isort-check:
 	poetry run isort --recursive --settings-path=isort.cfg --check-only
+
+type-check:
+	poetry run mypy -p gemini
 
 install:
 	poetry install
--- gemini/fmt.py
+++ gemini/fmt.py
@@ -5,13 +5,14 @@ def human_format_bytes(num: int) -> str:
     """Format an integer number of bytes as a human readable string.
 
     Args:
-        bytes_ (int): integer value of bytes to be formatted.
+        num (int): integer value of bytes to be formatted.
 
     Returns:
         str: Human readable string representing the number of bytes input.
     """
+    result = float(num)
     for unit in ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"]:
-        if abs(num) < float(1024):
-            return f"{num:3.2f} {unit}"
-        num /= 1024.0
-    return f"{num:.2f} YiB"
+        if abs(result) < float(1024):
+            return f"{result:3.2f} {unit}"
+        result /= 1024.0
+    return f"{result:.2f} YiB"
